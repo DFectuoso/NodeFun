830c07756c8f2eb700039952dac20cc84175461c
DFectuoso <santiago1717@gmail.com>
2010-01-16 19:56:35 -0800

    added 2 servers pinging each other

---
+++ example1.js
@@ -0,0 +1,36 @@
+var sys = require('sys'), 
+   http = require('http');
+
+function ping(port){
+  var request = http.createClient(port, "127.0.0.1").request("GET", "/");
+  sys.puts("i am trying" + port);
+  request.finish(function (response) {
+    response.addListener("body", function (chunk) {
+      response.setBodyEncoding("utf8");
+      sys.puts("BODY: " + chunk);
+    });
+  });  
+}
+
+
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    res.sendHeader(200, {'Content-Type': 'text/plain'});
+    res.sendBody('I am server 1');
+    res.finish();
+    ping(8001);
+  }, 500);
+}).listen(8000);
+
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    res.sendHeader(200, {'Content-Type': 'text/plain'});
+    res.sendBody('I am server 2');
+    res.finish();
+    ping(8000);
+  }, 500);
+}).listen(8001);
+
+setTimeout(function() {ping(8000);},2000);
+sys.puts('Server running at http://127.0.0.1:8000/');
+
